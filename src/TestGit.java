cfda400840dd4dda97277d81d41a1296f56aa206
Lenovo <Lenovo@DESKTOP-G49UM34>
2021-07-29 10:49:51 +0530

    First commit..

---
+++ src/TestGit.java
@@ -0,0 +1,12 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		boolean value=true;
+		if(value) {
+			System.out.println("Value is true : ");
+		}else {
+			System.out.println("Value is false : ");
+		}
+	}
+}
